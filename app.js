14dcbd22497a82c3fcb51f05d999a22393c479ec
Agetorp <elias.agetorp@gmail.com>
2016-02-26 15:54:26 +0100

    Added validator template

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var routes = require('./app/routes/index');
 var connection = require('./app/socket/socket');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use('/', routes);
 
@@ -10,7 +16,7 @@ connection.inits(io);
 
 var port = process.env.PORT || 3001;
 
-http.listen(port, function () {
+http.listen(port, function() {
   console.log('Eats API-server listening on port: ' + port);
 });
 
--- app/routes/restaurants.js
+++ app/routes/restaurants.js
@@ -2,18 +2,23 @@ var express = require('express');
 var router = express.Router();
 var restaurantHandler = require('../datahandlers/restaurants');
 var validator = require('../validators/restaurants');
+var valid = require('../validators/test');
 var connection = require('../socket/socket');
 
-router.get('/', validator.validateGet, function (req, res) {
+router.get('/', validator.validateGet, function(req, res) {
   connection.sendDataToId('id001', 'Someone connected to /restaurants');
   restaurantHandler
     .get(req)
-    .then(function (response) {
+    .then(function(response) {
       res.send(response.route);
     })
-    .catch(function (err) {
+    .catch(function(err) {
       res.status(500).send(err);
     });
+});
+
+router.post('/', valid.postBody, function(req, res) {
+  res.send(req.valid);
 });
 
 module.exports = router;

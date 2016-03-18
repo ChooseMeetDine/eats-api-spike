6fb5501d8f491fbc60bd51b41867ea7f07322115
David Josefson <david.josefson@gmail.com>
2016-03-18 21:41:59 +0100

    Removed aglio and fixed postgres and .env to support new staging server (digital ocean instead of heroku)

--- app/shared/knex.js
+++ app/shared/knex.js
@@ -1,14 +1,7 @@
 // This is currently only used for GET /users in the routes folder
 var pg = require('knex')({
   client: 'pg',
-  connection: {
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASS,
-    database: process.env.POSTGRES_DB,
-    port: process.env.POSTGRES_PORT,
-    host: process.env.POSTGRES_HOST,
-    ssl: true
-  },
+  connection: process.env.POSTGRES_CONNECTION,
   searchPath: 'knex,public',
   pool: {
     min: 1,
